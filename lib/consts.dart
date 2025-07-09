6baacdcc29e8563aeefa82a5b79c840618596f82
tehweee <pangtehwee11@gmail.com>
2025-07-10 03:34:41 +0800

    dsw

---
+++ lib/consts.dart
@@ -0,0 +1,2 @@
+const String GEMINI_API_KEY = "AIzaSyBT5-OlhzWBwp5sjbEDGZyz6R3cPX-6m7M";
+
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart'; // Your designed homepage
 import 'package:profile_test_isp/pages/ItineraryPage.dart';
 import 'package:profile_test_isp/pages/ProfilePage.dart';
 import 'pages/LoadingPage.dart';
+import 'consts.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 
 void main() {
+  Gemini.init(apiKey: GEMINI_API_KEY);
   runApp(MaterialApp(
     home: ItineraryPage(),
   ));
