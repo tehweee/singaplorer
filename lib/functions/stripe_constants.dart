01b800000374aa3442f92d77cc45f46040f46eb9
XPsalter <altpsykic162@gmail.com>
2025-07-10 10:26:14 +0800

    pubspec yaml update and Stripe Constants added

---
+++ lib/functions/stripe_constants.dart
@@ -0,0 +1,4 @@
+const apiKey =
+    "pk_test_51RWSiyR80T9GIRjYUAcUIXrUutCGXHbecsKlF6x52ssmT0jhuHZYR8O8D8YAvOVKlTEinBe5AOWTyVqlEduCh5fl008WQo0waR";
+
+const priceAIToken = "price_1RgddSR80T9GIRjY01vSutuQ";
