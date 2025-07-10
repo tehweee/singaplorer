99e7babcf7cb1f9fbad3eb364c06cced58d5ff7c
tehweee <pangtehwee11@gmail.com>
2025-07-10 12:29:32 +0800

    dwdw

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart'; // Your designed homepage
-import 'package:profile_test_isp/pages/AIChatPage.dart';
 import 'package:profile_test_isp/pages/ItineraryPage.dart';
 import 'package:profile_test_isp/pages/ProfilePage.dart';
 import 'pages/LoadingPage.dart';
