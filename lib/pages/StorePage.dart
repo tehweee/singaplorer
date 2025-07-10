9c090edf754f428c7a6357deea6c894b8782511b
XPsalter <altpsykic162@gmail.com>
2025-07-10 12:37:19 +0800

    Create StorePage.dart

---
+++ lib/pages/StorePage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../functions/payment_service.dart'
+
+class StorePage extends StatefulWidget {
+  const StorePage({super.key});
+
+  @override
+  State<StorePage> createState() => _StorePageState();
+}
+
+class _StorePageState extends State<StorePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
